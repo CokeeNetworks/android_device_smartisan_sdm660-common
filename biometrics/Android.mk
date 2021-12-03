d020177b3c2ace8882dd8bd7c844c357cf77f1ed
Roman Kiryanov <rkir@google.com>
2021-12-03 14:06:44 +0800

    sdm660-common: biometrics: Add vintf_fragments to android.hardware.biometrics.fingerprint@2.1-service
    * adapt to device specific file extension
    
    Bug: 144386336
    Test: boot emulator, lshal | grep fingerprint
    Merged-In: I97ad0451bb3edfe85d33300baaa41c3fbc5c3cc6
    Change-Id: I11c8ebea731f5e7b24ee483d20a8cd25fd01be38

--- biometrics/Android.mk
+++ biometrics/Android.mk
@@ -3,6 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := android.hardware.biometrics.fingerprint@2.1-service.smartisan
 LOCAL_INIT_RC := android.hardware.biometrics.fingerprint@2.1-service.smartisan.rc
+LOCAL_VINTF_FRAGMENTS := android.hardware.biometrics.fingerprint@2.1-service.smartisan.xml
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SRC_FILES := \
