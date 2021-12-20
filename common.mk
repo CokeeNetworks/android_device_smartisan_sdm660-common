7610faba37476fb7f6bbbaf6d669c94abac87ea2
Wang Han <416810799@qq.com>
2021-12-20 16:45:33 +0800

    sdm660-common: Import gpfspath_oem_config.xml
    * Opened by libdrmfs.so. Add it to be safe better than sorry.
    
    Change-Id: I46f33fba4591750a3c932e15b2b80a3c071ee4dd

--- common.mk
+++ common.mk
@@ -383,6 +383,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libjson
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/gpfspath_oem_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/gpfspath_oem_config.xml
+
 # Releasetools
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/bin/deunify.sh:install/bin/deunify.sh
