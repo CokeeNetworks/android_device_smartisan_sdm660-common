fb7c7f1afa8954fef3a3727199fd6abfae04a7b7
Alexander Koskovich <akoskovich@pm.me>
2022-03-06 19:07:04 +0800

    sdm660-common: Drop support for Snapdragon Camera.
     * Won't be implemented this cycle (or ever again hopefully).
    
    Change-Id: Ie2a346319dfe814deaf9833cb31640505cc6b7dc

--- common.mk
+++ common.mk
@@ -165,9 +165,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service
 
-PRODUCT_PACKAGES += \
-    Snap
-
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
     cneapiclient \
