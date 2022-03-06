d481ee042993003b18fe4f59870b807c104244de
anrui2032 <anrui2032@gmail.com>
2022-03-06 18:26:10 +0800

    [DNM] sdm660-common: SELinux igonre neverallows
    Change-Id: Ifb509e8a11aedc12fbcfc416b2c42e8cc36449b6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -190,6 +190,7 @@ VENDOR_SECURITY_PATCH := 2019-10-10
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
