6602b73a77c0b9e1cd5e60f6944b16ffd6b06fb6
Kuber Sharma <kubersharma001@gmail.com>
2022-05-02 03:56:14 +0000

    sdm660-common: Ship GCamGOPrebuilt instead of Camera2/Snap
     * works with/without GAPPS.
    
    Change-Id: I3cb2677e579e588a637e112687f5838ecc6a5b10

--- common.mk
+++ common.mk
@@ -172,6 +172,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service
 
+PRODUCT_PACKAGES += \
+    GCamGOPrebuilt
+
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
