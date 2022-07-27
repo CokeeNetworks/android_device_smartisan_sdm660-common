f100c5c0512d6cb598580ea0f28cfca0ccd3a685
anrui2032 <anrui2032@gmail.com>
2022-07-27 15:00:58 +0800

    sdm660-common: Build kernel with Clang
    Signed-off-by: anrui2032 <anrui2032@gmail.com>
    Change-Id: I6284e72ac913ba73847ff245f5d1a6d98a5cf2b0

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -64,6 +64,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/smartisan/sdm660
 TARGET_KERNEL_CONFIG := smartisan_sdm660_defconfig
+TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
